6cb8eebf11a88d25c8e0604d3459ea6b3f204f65
Guy Wilson <wilson.guy@gmail.com>
2023-09-20 17:57:08 +0100

    Got unformatted bin output working

--- src/utils.c
+++ src/utils.c
@@ -5,20 +5,24 @@
 
 #include "utils.h"
 
-#define BASE2_OUTPUT_LEN                        ((sizeof(uint32_t) * 8) + 1)
+#define BASE2_OUTPUT_LEN                        (sizeof(uint32_t) * 8)
 
 const char * getBase2String(uint32_t value) {
-    char        szOutputString[BASE2_OUTPUT_LEN];
+    char        szOutputString[BASE2_OUTPUT_LEN + 1];
     int         i = BASE2_OUTPUT_LEN - 1;
     int         bit = 0;
+
+    szOutputString[BASE2_OUTPUT_LEN] = 0;
 
     while (value > 0) {
         bit = value % 2;
 
         value = value >> 1;
 
-        szOutputString[i--] = (char)bit;
+        szOutputString[i--] = ((char)bit + '0');
     }
+
+    strncpy(szOutputString, &szOutputString[i + 1], BASE2_OUTPUT_LEN);
 
     return strndup(szOutputString, BASE2_OUTPUT_LEN);
 }
